7fb84e5228a3a92753e9d568130dab5807731e1b
James Sparham <jsparham777@gmail.com>
2019-02-02 13:24:33 +0000

    Added a logger to the DataAccess constructor

--- DemoLibrary/Utilities/DataAccess.cs
+++ DemoLibrary/Utilities/DataAccess.cs
@@ -4,14 +4,23 @@ namespace DemoLibrary.Utilities
 {
     public class DataAccess : IDataAccess
     {
+        private readonly ILogger _logger;
+
+        public DataAccess(ILogger logger)
+        {
+            _logger = logger;
+        }
+
         public void LoadData()
         {
             Console.WriteLine("Loading Data");
+            _logger.Log("Loading data");
         }
 
         public void SaveData(string name)
         {
             Console.WriteLine($"Saving {name}");
+            _logger.Log("Saving data");
         }
     }
 }
